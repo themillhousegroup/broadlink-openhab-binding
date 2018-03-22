13c79e9f1ef385960edce5b1c1b02bf965f161c9
John Marshall <john@themillhousegroup.com>
2018-03-22 22:39:41 +1100

    Begin porting JAD files to Java

---
+++ src/main/java/org/openhab/binding/broadlink/config/BroadlinkControllerConfig.java
@@ -0,0 +1,18 @@
+// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+// Source File Name:   BroadlinkControllerConfig.java
+
+package org.openhab.binding.broadlink.config;
+
+
+public class BroadlinkControllerConfig
+{
+
+    public BroadlinkControllerConfig()
+    {
+    }
+
+    public String host;
+    public int heartBeatInterval;
+}
